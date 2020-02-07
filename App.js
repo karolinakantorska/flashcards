345c42899cf53a186488f05f1c063cc457c48f09
Your Name <karolina.kantorska@gmail.com>
2020-02-07 13:00:17 +0100

    update the options configuration for the active screen from the mounted screen component itself

--- App.js
+++ App.js
@@ -26,17 +26,21 @@ function DetailsScreen({ route, navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details', {
+          itemId: Math.floor(Math.random() * 100),
+        })
         }
-        />
-        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-        <Button title="Go back" onPress={() => navigation.goBack()} />
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Update the title"
+        onPress={() => navigation.setOptions({ title: 'Updated!' })}
+/>
     </View>
   );
 }
