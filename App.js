038a892928ad35c447e1b61f95764859a4453487
Your Name <karolina.kantorska@gmail.com>
2020-02-07 12:44:45 +0100

    params in rout

--- App.js
+++ App.js
@@ -8,26 +8,39 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
     </View>
   );
 }
-function DetailsScreen({ navigation }) {
+function DetailsScreen({ route, navigation }) {
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
         <Button
           title="Go to Details... again"
-          onPress={() => navigation.navigate('Details')}
+          onPress={() => navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
         />
         <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
         <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
+
 
 const Stack = createStackNavigator();
 
