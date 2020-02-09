0b66b3469c93eb9c35e72fb7295e5b3bcb0c3823
Your Name <karolina.kantorska@gmail.com>
2020-02-09 22:02:15 +0100

    quiz working

--- components/QuizView.js
+++ components/QuizView.js
@@ -4,43 +4,41 @@ import { View, StyleSheet, Text, Button } from 'react-native'
 import { connect } from "react-redux";
 import QuizQuestionCard from './QuizQuestionCard'
 
-function quiz (questions) {
-  return
-}
-
-
-
 class QuizView extends Component {
   state = {
     ansQ: 0,
+    correct: 0
   }
   render () {
     const { id, questions, nrQuestions } = this.props
     return (
       <View>
-        <View>
-          <Text>Quiz: {id}</Text>
-          <Text> {this.state.ansQ} / {nrQuestions}</Text>
-        </View>
-        <View>
-
-          <QuizQuestionCard  Que={questions[this.state.ansQ]}/>
-          <Button title='correct' onPress={(state) => this.setState({ansQ: this.state.ansQ +1})}/>
-          <Button title='incorrect' />
-        </View>
+        { (this.state.ansQ === nrQuestions) ?
+          (<Text style={{fontSize: 20 }}>correct: {this.state.correct} / {nrQuestions}</Text>)
+        :
+        (<View>
+          <View>
+            <Text>Quiz: {id}</Text>
+            <Text> {this.state.ansQ} / {nrQuestions}</Text>
+            <Text>correct: {this.state.correct}</Text>
+          </View>
+          <View>
+            <QuizQuestionCard  Que={questions[this.state.ansQ]}/>
+            <Button title='correct' onPress={(state) => this.setState({ansQ: this.state.ansQ +1, correct: this.state.correct +1 })}/>
+            <Button title='incorrect' onPress={(state) => this.setState({ansQ: this.state.ansQ +1})}/>
+          </View>
+        </View>)
+        }
       </View>
+
     )
   }
 }
-
-
-
 function mapStateToProps(state, {id}) {
   const questions = state[id].questions
-  const questionOne = questions[0].question
 
   const questionKeyList = Object.keys(state)
-  const nrQuestions = questionKeyList.length
+  const nrQuestions = questionKeyList.length + 1
 
   return {
     id,
